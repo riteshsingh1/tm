e596b394a12c90a9a0c3fb51261edf2bb402e9da
Ritesh Singh <hello@imritesh.com>
2020-11-26 14:00:54 +0530

    initial commit

---
+++ database/migrations/2020_11_26_074435_create_translations_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTranslationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('translations', function (Blueprint $table) {
+            $table->id();
+            $table->string('source');
+            $table->string('target');
+            $table->text('sentence');
+            $table->text('translation');
+            $table->integer('clientId');
+            $table->string('domain');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('translations');
+    }
+}
